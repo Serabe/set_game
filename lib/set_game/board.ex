f7a9bd2b028420655891295f7c98296c4ed0fec7
Sergio Arbeo <serabe@gmail.com>
2020-08-09 20:41:20 +0200

    First models for board and cards

---
+++ lib/set_game/board.ex
@@ -0,0 +1,56 @@
+defmodule SetGame.Board do
+  defstruct deck: [], table: []
+
+  @doc """
+  Generate a new board
+
+  ## Example
+      iex> %SetGame.Board{} = SetGame.Board.new()
+  """
+  def new() do
+    %SetGame.Board{
+      deck: Enum.shuffle(0..80),
+      table: []
+    }
+  end
+
+  @doc """
+  Deals cards from the top of the deck
+
+  ## Example
+      iex> %SetGame.Board{} = SetGame.Board.deal(SetGame.Board.new())
+  """
+  def deal(
+        %SetGame.Board{deck: deck, table: table} = board,
+        cards_to_deal \\ nil
+      ) do
+    cards_to_deal = cards_to_deal || number_of_cards_to_deal(board)
+
+    %SetGame.Board{
+      deck: Enum.drop(deck, cards_to_deal),
+      table: update_table(table, Enum.take(deck, cards_to_deal))
+    }
+  end
+
+  defp update_table(table, new_elements) do
+    Enum.reduce(new_elements, table, &add_element_to_table/2)
+  end
+
+  defp add_element_to_table(element, table) do
+    case Enum.find_index(table, &is_nil/1) do
+      nil -> List.insert_at(table, -1, element)
+      index -> List.update_at(table, index, fn _ -> element end)
+    end
+  end
+
+  def number_of_cards_to_deal(%SetGame.Board{table: table}) do
+    cards_on_table = Enum.count(table)
+
+    cond do
+      # If we have more than 12 cards, we complete to the nearest multiple of 3
+      cards_on_table >= 12 -> 3 - rem(cards_on_table, 3)
+      # Otherwise we complete to 12 cards
+      true -> 12 - cards_on_table
+    end
+  end
+end
---
+++ test/set_game/board_test.exs
@@ -0,0 +1,119 @@
+defmodule SetGame.BoardTest do
+  use ExUnit.Case
+  doctest SetGame.Board
+
+  alias SetGame.Board
+
+  describe "#new" do
+    test "generates a new board" do
+      board = Board.new()
+
+      assert Enum.count(board.deck) == 81
+      assert Enum.all?(board.deck, &(&1 < 81))
+      assert Enum.count(board.table) == 0
+    end
+  end
+
+  describe "deal/1" do
+    test "after a normal set recollection, deal the top three cards on the table" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..19)
+      }
+
+      after_deal = Board.deal(board)
+
+      assert Enum.count(after_deal.deck) == 2
+      assert after_deal.deck == [5, 6]
+      assert Enum.count(after_deal.table) == 12
+      assert Enum.member?(after_deal.table, 1)
+      assert Enum.member?(after_deal.table, 2)
+      assert Enum.member?(after_deal.table, 3)
+    end
+
+    test "if there are an _odd_ number of cards, complete until 12" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..18)
+      }
+
+      after_deal = Board.deal(board)
+
+      assert Enum.count(after_deal.deck) == 1
+      assert after_deal.deck == [6]
+      assert Enum.count(after_deal.table) == 12
+      assert Enum.member?(after_deal.table, 1)
+      assert Enum.member?(after_deal.table, 2)
+      assert Enum.member?(after_deal.table, 3)
+      assert Enum.member?(after_deal.table, 5)
+    end
+
+    test "if there are 12 cards, deal 3 more" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..22)
+      }
+
+      after_deal = Board.deal(board)
+
+      assert Enum.count(after_deal.deck) == 2
+      assert after_deal.deck == [5, 6]
+      assert Enum.count(after_deal.table) == 15
+      assert Enum.member?(after_deal.table, 1)
+      assert Enum.member?(after_deal.table, 2)
+      assert Enum.member?(after_deal.table, 3)
+    end
+
+    test "if there are 13 cards, deal 2 more" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..23)
+      }
+
+      after_deal = Board.deal(board)
+
+      assert Enum.count(after_deal.deck) == 3
+      assert after_deal.deck == [3, 5, 6]
+      assert Enum.count(after_deal.table) == 15
+      assert Enum.member?(after_deal.table, 1)
+      assert Enum.member?(after_deal.table, 2)
+    end
+
+    test "if there are 17 cards, deal 1 more" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..27)
+      }
+
+      after_deal = Board.deal(board)
+
+      assert Enum.count(after_deal.deck) == 4
+      assert after_deal.deck == [2, 3, 5, 6]
+      assert Enum.count(after_deal.table) == 18
+      assert Enum.member?(after_deal.table, 1)
+    end
+
+    test "if there are empty positions in the table, fill them" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: [11, 12, nil, 14, 15, nil, nil, 18, 19, 20, 21, 22]
+      }
+
+      after_deal = Board.deal(board)
+
+      assert after_deal.table == [11, 12, 1, 14, 15, 2, 3, 18, 19, 20, 21, 22]
+    end
+
+    test "the second parameter modifies the number of cards dealt" do
+      board = %Board{
+        deck: [1, 2, 3, 5, 6],
+        table: Enum.to_list(11..19)
+      }
+
+      after_deal = Board.deal(board, 4)
+
+      assert after_deal.deck == [6]
+      assert after_deal.table == Enum.to_list(11..19) ++ [1, 2, 3, 5]
+    end
+  end
+end
