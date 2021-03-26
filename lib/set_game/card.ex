50b55d4a6023f50b23af1dff53b83cd17e160b44
Sergio Arbeo <serabe@gmail.com>
2021-03-26 21:27:12 +0100

    Document card implementation

--- lib/set_game/card.ex
+++ lib/set_game/card.ex
@@ -1,4 +1,17 @@
 defmodule SetGame.Card do
+  @moduledoc """
+  Cards in SET have four properties with three different values each. Therefore, the simplest
+  way to represent a card (and generate all possible values) is represint it with an
+  integer between 1 and 80 (80 = 3 ^ 4 - 1).
+
+  If we want to generate cards based on values for each property, we can use `Integer.undigits`,
+  that will parse and array of numbers as a number in the base given as the second parameter.
+  `Integer.undigits([1, 0], 2)`  will return `2`, `Integer.undigits([1, 0], 3)` will return `3`
+  and so on (remember, all bases are 10 in that same base :P).
+
+  Given an integer we can get the digits in base 3 by using `unundigits => digits`.
+  """
+
   @doc """
   Given three cards, returns whether they are a valid set or not.
 
@@ -57,6 +70,9 @@ defmodule SetGame.Card do
     |> Enum.all?(&are_props_a_set?/1)
   end
 
+  # Three properties satisfy the set condition (either all are the equal or all different) if
+  # their sum is congruent modulo 3, which is the fancy mathematical term to mean the sum is
+  # divisible by 3
   defp are_props_a_set?({a, b, c}) do
     rem(a + b + c, 3) === 0
   end
@@ -64,6 +80,7 @@ defmodule SetGame.Card do
   defp num_to_props(n) do
     digits = n |> rem(81) |> Integer.digits(3)
 
+    # Normalize the length of the array (always lead with 0).
     case Enum.count(digits) do
       1 ->
         [0, 0, 0] ++ digits
